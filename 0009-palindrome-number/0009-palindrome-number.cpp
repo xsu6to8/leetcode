28ed73c1f9482e183340ea9db93194ede6d36d20
xsu6to8 <xsu6to8@gmail.com>
2025-12-30 18:24:04 +0900

    Time: 0 ms (100%), Space: 8.4 MB (91.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        for(int i = 0; i < s.size()/2; i++){
-            if(s[i] != s[s.size() - 1 - i])
-                return false;
-        }   
-        return true;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 };
