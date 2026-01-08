099759d7cefa16d95bec2c0fd05692d86a99063c
xsu6to8 <xsu6to8@gmail.com>
2026-01-08 12:26:02 +0900

    Time: 0 ms (100%), Space: 10.1 MB (25.32%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        
+        while(i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            
+            if(i >= 0) {
+                sum += a[i] - '0'; 
+                i--;
+            }
+            if(j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+            
+            res += to_string(sum % 2);
+            
+            carry = sum / 2;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
