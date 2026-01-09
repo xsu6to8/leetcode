ddac17a048f2fd3b7fc86a4dfdc1e94f8648e0eb
xsu6to8 <xsu6to8@gmail.com>
2026-01-09 10:44:26 +0900

    Time: 0 ms (100%), Space: 20.5 MB (82.62%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) 
-    {
-        sort(nums.begin(), nums.end());
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int n : nums) 
+            result ^= n; 
 
-        for(auto i : nums)
-        {
-            int gap = upper_bound(nums.begin(), nums.end(), i) - lower_bound(nums.begin(), nums.end(), i);
-
-            if(gap == 1)
-            {
-                return i;
-            }
-        }
-        return 0;
+        return result; 
     }
 };
