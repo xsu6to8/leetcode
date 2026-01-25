c98ef510551c9dccf99babf386a567baba7f2dc8
xsu6to8 <xsu6to8@gmail.com>
2026-01-25 16:42:26 +0900

    Time: 24 ms (37.87%), Space: 13.3 MB (26.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,12 @@
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]struct{})
+
+    for _, num := range nums {
+        if _, exists := seen[num]; exists {
+            return true
+        }
+        seen[num] = struct{}{}
+    }
+
+    return false
+}
