98c926ea772a490144c70186f172f653d6be033a
xsu6to8 <xsu6to8@gmail.com>
2026-01-27 10:53:09 +0900

    Time: 541 ms (47.35%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+WITH tmp AS (
+    SELECT num, 
+       LEAD(num, 1) OVER (ORDER BY id) AS next_1,
+       LEAD(num, 2) OVER (ORDER BY id) AS next_2
+    FROM Logs
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM tmp
+WHERE num = next_1 AND num = next_2;
