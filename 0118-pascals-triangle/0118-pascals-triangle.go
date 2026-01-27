eacd9bd671374e1e44eea040067c92e2720fe9a4
xsu6to8 <xsu6to8@gmail.com>
2026-01-27 22:19:56 +0900

    Time: 0 ms (100%), Space: 4.3 MB (51.73%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.go
@@ -0,0 +1,18 @@
+func generate(numRows int) [][]int {
+    res := make([][]int, numRows)
+
+    for i := 0; i < numRows; i++ {
+        row := make([]int, i+1)
+
+        row[0], row[i] = 1, 1
+
+        // i >= 2 case
+        for j := 1; j < i; j++ {
+            row[j] = res[i-1][j-1] + res[i-1][j]
+        }
+
+        res[i] = row
+    }
+
+    return res
+}
