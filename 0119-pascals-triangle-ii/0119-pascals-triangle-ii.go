48ea09223562ac10a788a1c6b67d1c3caf7ba615
qxsut <xsu6to8@gmail.com>
2026-02-03 12:12:11 +0900

    Time: 0 ms (100%), Space: 3.9 MB (74.49%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.go
@@ -0,0 +1,13 @@
+func getRow(rowIndex int) []int {
+    res := make([]int, rowIndex + 1)
+
+    res[0] = 1
+
+    for i := 1; i <= rowIndex; i++ {
+        for j := i; j >= 1; j-- {
+            res[j] = res[j] + res[j-1]
+        }
+    }
+
+    return res
+}
