f911225eccebae73ad272b056fe3d128d3a50905
qxsut <xsu6to8@gmail.com>
2026-02-24 16:26:11 +0900

    Time: 2 ms (5.36%), Space: 4.1 MB (54.17%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.go
@@ -0,0 +1,25 @@
+func isUgly(n int) bool {
+    if n <= 0 {
+        return false
+    } else if n == 1 {
+        return true
+    }
+
+    for n%2 == 0{
+        n /= 2
+    }
+    for n%3 == 0{
+        n /= 3
+    }
+    for n%5 == 0{
+        n /= 5
+    }
+
+    if n == 1 {
+        return true
+    } else if n == 0 {
+        return false
+    }
+
+    return false
+}
