7ed4102239e549fdd8e3f8c8325f80fdd90096d6
qxsut <xsu6to8@gmail.com>
2026-03-10 21:29:53 +0900

    Time: 0 ms (100%), Space: 8.9 MB (43.9%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,12 @@
+func moveZeroes(nums []int)  {
+    pos := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[pos] = nums[i] 
+            pos++
+        } 
+    }
+    for ; pos < len(nums); pos++ {
+        nums[pos] = 0
+    }
+}
