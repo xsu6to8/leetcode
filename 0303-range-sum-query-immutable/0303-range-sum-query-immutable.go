fd9e11b40a6750eba430aebd249d95194b46eb1f
qxsut <xsu6to8@gmail.com>
2026-03-24 14:36:41 +0900

    Time: 0 ms (100%), Space: 11 MB (21.93%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.go
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.go
@@ -1,27 +1,47 @@
-type NumArray struct {
-    arr []int
-}
+// type NumArray struct {
+//     arr []int
+// }
 
+
+// func Constructor(nums []int) NumArray {
+//     na := &NumArray{nums}
+//     return *na
+// }
+
+
+// func (this *NumArray) SumRange(left int, right int) int {
+//     sum := 0
+//     for left <= right {
+//         sum += this.arr[left]
+//         left++
+//     }
+
+//     return sum
+// }
+
+
+// /**
+//  * Your NumArray object will be instantiated and called as such:
+//  * obj := Constructor(nums);
+//  * param_1 := obj.SumRange(left,right);
+//  */
+
+
+// 최적화 - 누적합 => {O(n) -> O(1)}
+type NumArray struct {
+    sums []int
+}
 
 func Constructor(nums []int) NumArray {
-    na := &NumArray{nums}
-    return *na
+    n := len(nums)
+    // 계산 편의를 위해 길이를 n+1로 설정 (0번째 인덱스는 0)
+    sums := make([]int, n+1)
+    for i := 0; i < n; i++ {
+        sums[i+1] = sums[i] + nums[i]
+    }
+    return NumArray{sums: sums}
 }
-
 
 func (this *NumArray) SumRange(left int, right int) int {
-    sum := 0
-    for left <= right {
-        sum += this.arr[left]
-        left++
-    }
-
-    return sum
+    return this.sums[right+1] - this.sums[left]
 }
-
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * obj := Constructor(nums);
- * param_1 := obj.SumRange(left,right);
- */
