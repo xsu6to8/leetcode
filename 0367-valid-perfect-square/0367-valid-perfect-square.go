52788ba854dea28872f1349d1159ca2e9fadd62b
qxsut <xsu6to8@gmail.com>
2026-03-30 11:52:01 +0900

    Time: 0 ms (100%), Space: 3.8 MB (99.04%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.go
@@ -0,0 +1,11 @@
+func isPerfectSquare(num int) bool {
+    for i := 1; ; i++ {
+        if i * i < num {
+            continue
+        } else if i * i == num {
+            return true
+        } else if i * i > num {
+            return false
+        }
+    }
+}
