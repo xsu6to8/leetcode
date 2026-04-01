f309e7f8ca595a4b5f0cf634a61b5157e4e3cf66
qxsut <xsu6to8@gmail.com>
2026-04-01 13:58:38 +0900

    Time: 6 ms (64.46%), Space: 5.9 MB (41.07%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.go
+++ 0383-ransom-note/0383-ransom-note.go
@@ -20,3 +20,12 @@ func canConstruct(ransomNote string, magazine string) bool {
 
     return true
 }
+
+/*
+이 문제 국한하자면 array [26]int로 가고
+ransom은 + / magazine은 - 역할만 수행하며
+중간에 negative 값 띄면 바로 false
+마지막에 다 0인지 확인하는 게 더 효율적
+
+만일 근데 utf-8 모든 유니코드 수용이라면 map 방식이 좋음
+*/
