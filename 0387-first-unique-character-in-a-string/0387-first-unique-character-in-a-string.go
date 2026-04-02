061ee08bd481440e0a0f807c82b2fb5cc8e25989
qxsut <xsu6to8@gmail.com>
2026-04-02 13:41:09 +0900

    Time: 24 ms (49.59%), Space: 8.1 MB (50.55%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -13,3 +13,6 @@ func firstUniqChar(s string) int {
 
     return -1
 }
+
+// 1. 지금처럼 알파벳 only일 땐 map보다 [26]int 배열이 유리 (해시 연산/메모리 할당 오버헤드 제거)
+// 2. 배열은 연속된 메모리 구조로 cache hit가 높아 map보다 실제 실행 속도가 훨씬 빠름
