b471de1f36ce3452c7487b18c25e20c96af5d01f
qxsut <xsu6to8@gmail.com>
2026-07-18 22:59:54 +0900

    Time: 0 ms (100%), Space: 8.6 MB (88.81%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.go
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.go
@@ -1,15 +1,39 @@
-func removeStars(s string) string {
-    var resStack []rune
+// func removeStars(s string) string {
+//     var resStack []rune
 
-    for _, v := range(s) {
-        if v != '*' {
-            resStack = append(resStack, v)
+//     for _, v := range(s) {
+//         if v != '*' {
+//             resStack = append(resStack, v)
+//         } else {
+//             top := len(resStack) - 1
+// 		    resStack = (resStack)[:top]   // 스택에 마지막 데이터 제거함
+//         }
+//     }
+
+//     res := string(resStack)
+//     return res
+// }
+
+
+// 성능 개선 시, 투 포인터 가능
+func removeStars(s string) string {
+    b := []byte(s)
+    
+    // 값을 새로 쓸 위치를 가리키는 포인터 (스택의 top 역할)
+    writeIdx := 0
+    
+    for i := 0; i < len(b); i++ {
+        if b[i] == '*' {
+            // 별을 만나면 쓰기 포인터를 하나 다시 앞으로 (정상 진행 방향 ->)
+            if writeIdx > 0 {
+                writeIdx--
+            }
         } else {
-            top := len(resStack) - 1
-		    resStack = (resStack)[:top]   // 스택에 마지막 데이터 제거함
+            // 일반 문자면 현재 쓰기 위치에 값을 덮어쓰고 포인터 뒤로 (반대 진행 방향 <-)
+            b[writeIdx] = b[i]
+            writeIdx++
         }
     }
 
-    res := string(resStack)
-    return res
+    return string(b[:writeIdx])
 }
