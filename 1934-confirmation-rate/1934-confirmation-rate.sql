34c7258ad0c86458db207fe93eb2e934ac06b645
qxsut <xsu6to8@gmail.com>
2026-07-23 22:22:15 +0900

    Time: 733 ms (30.95%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,10 @@
+SELECT 
+    s.user_id,
+    ROUND(
+        IFNULL(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 0), 
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c 
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
