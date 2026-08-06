73a3e94a2db27e362a53f2dd7e1f8457e0a93b65
qxsut <xsu6to8@gmail.com>
2026-08-06 22:34:09 +0900

    Time: 1 ms (64.84%), Space: 7.8 MB (66.41%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.go
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.go
@@ -1,16 +1,28 @@
+// func repeatedSubstringPattern(s string) bool {
+//     testStr := s + s
+//     testStr = testStr[1:len(testStr)-1]
+
+//     originLen := len(s)
+//     testLen := len(testStr)
+
+//     isMatched := false
+//     // maximum : O(2N)
+//     for i := 0; i <= testLen - originLen; i++ {
+//         // inner logic -> O(N) 일일히 비교
+//         if s == testStr[i : i + originLen] {
+//             isMatched = true
+//         }
+//     }
+
+//     return isMatched
+// }
+
+
 func repeatedSubstringPattern(s string) bool {
-    testStr := s + s
-    testStr = testStr[1:len(testStr)-1]
-
-    originLen := len(s)
-    testLen := len(testStr)
-
-    isMatched := false
-    for i := 0; i <= testLen - originLen; i++ {
-        if s == testStr[i : i + originLen] {
-            isMatched = true
-        }
-    }
-
-    return isMatched
+    // s + s 생성 후 양 끝 1글자씩 제거
+    // -> 같은 문자열 존재 시, [one cycle 돌리면 한번은 나오게 되어있음]
+    testStr := (s + s)[1 : len(s)*2-1]
+    
+    // strings.Contains 함수 -> 부분 문자열 포함 여부 검사 O(N)
+    return strings.Contains(testStr, s)
 }
