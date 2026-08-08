475db6446afa63f7b75141b9da857a7a86393724
qxsut <xsu6to8@gmail.com>
2026-08-08 16:32:08 +0900

    Time: 0 ms (100%), Space: 4 MB (29.41%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.go
+++ 0461-hamming-distance/0461-hamming-distance.go
@@ -1,13 +1,18 @@
+// func hammingDistance(x int, y int) int {
+//     diff := x ^ y
+//     toBin := strconv.FormatInt(int64(diff), 2)
+
+//     cnt := 0
+//     for _, v := range toBin {
+//         if v == '1' {
+//             cnt++
+//         }
+//     }
+
+//     return cnt
+// }
+
+// Go library [math/bits] 사용 ->  bits.OnesCount
 func hammingDistance(x int, y int) int {
-    diff := x ^ y
-    toBin := strconv.FormatInt(int64(diff), 2)
-
-    cnt := 0
-    for _, v := range toBin {
-        if v == '1' {
-            cnt++
-        }
-    }
-
-    return cnt
+    return bits.OnesCount(uint(x ^ y))
 }
