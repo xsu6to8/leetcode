145e958f7fa2bdd32a7e5c4bcb261bc70c074580
qxsut <xsu6to8@gmail.com>
2026-09-01 22:13:03 +0900

    Time: 0 ms (100%), Space: 4 MB (6.9%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.go
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.go
@@ -1,18 +1,23 @@
+// func checkRecord(s string) bool {
+//     aCnt := 0
+
+//     for i := 0; i < len(s); i++ {
+//         if s[i] == 'A' {
+//             aCnt++
+//             if aCnt == 2 {
+//                 return false
+//             }
+//         } else if s[i] == 'L' {
+//             if i+1 < len(s) && i+2 < len(s) && s[i+1] == 'L' && s[i+2] == 'L' {
+//                 return false
+//             }
+//         }
+//     }
+
+//     return true
+// }
+
+// 전부 내장함수로 해결하는 version
 func checkRecord(s string) bool {
-    aCnt := 0
-
-    for i := 0; i < len(s); i++ {
-        if s[i] == 'A' {
-            aCnt++
-            if aCnt == 2 {
-                return false
-            }
-        } else if s[i] == 'L' {
-            if i+1 < len(s) && i+2 < len(s) && s[i+1] == 'L' && s[i+2] == 'L' {
-                return false
-            }
-        }
-    }
-
-    return true
+    return strings.Count(s, "A") < 2 && !strings.Contains(s, "LLL")
 }
